3de99bf3faf7266ed327cce839a863a8e34caeda
carlafuschino <carlafuschino@hotmail.com>
2024-06-17 19:54:26 -0300

    nuevos cambios-finales

--- js/app.js
+++ js/app.js
@@ -59,3 +59,28 @@ async function handleSubmit(event) {
     this.reset();
     bootstrapModal.show();
 }
+
+// Get the button
+let backToTopButton = document.getElementById('back-to-top-btn');
+
+// Show the button when the user scrolls down 20px from the top of the document
+window.onscroll = function () {
+  scrollFunction();
+};
+
+function scrollFunction() {
+  if (
+    document.body.scrollTop > 20 ||
+    document.documentElement.scrollTop > 20
+  ) {
+    backToTopButton.style.display = 'block';
+  } else {
+    backToTopButton.style.display = 'none';
+  }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+backToTopButton.addEventListener('click', function () {
+  document.body.scrollTop = 0; // For Safari
+  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE, and Opera
+});
